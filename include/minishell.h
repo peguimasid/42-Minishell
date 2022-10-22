7fe8ff065e109502596395faeacf33b9178d3bf6
Guilhermo Masid <guilhermomasid@gmail.com>
2022-10-22 17:35:31 -0300

    feature: accepting prompt from user

---
+++ include/minishell.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gmasid <gmasid@student.42.rio>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/05 15:16:25 by gmasid            #+#    #+#             */
+/*   Updated: 2022/10/22 17:34:20 by gmasid           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+#include <readline/readline.h>
+#include <readline/history.h>
+#include <signal.h>
+#include <stdio.h>
+
+#endif
