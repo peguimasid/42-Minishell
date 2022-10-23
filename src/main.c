e3fd53e010a5ccf3e9a8755290b6a224ff59c080
Guilhermo Masid <guilhermomasid@gmail.com>
2022-10-23 12:50:28 -0300

    get env variables

--- src/main.c
+++ src/main.c
@@ -6,16 +6,18 @@
 /*   By: gmasid <gmasid@student.42.rio>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/22 17:31:19 by gmasid            #+#    #+#             */
-/*   Updated: 2022/10/22 17:34:18 by gmasid           ###   ########.fr       */
+/*   Updated: 2022/10/23 12:50:11 by gmasid           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/minishell.h"
 
-int	main(void)
+int	main(int argc, char **argv, char **env)
 {
 	char	*line;
 
+	(void)argc;
+	(void)argv;
 	while (1)
 	{
 		line = readline("Minishell ▸ ");
