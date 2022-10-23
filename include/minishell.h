81f92f89cc34c9bf316e01b1ef162d9224be678e
Guilhermo Masid <guilhermomasid@gmail.com>
2022-10-23 12:52:06 -0300

    fix: norm in minishell.h

--- include/minishell.h
+++ include/minishell.h
@@ -6,16 +6,16 @@
 /*   By: gmasid <gmasid@student.42.rio>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/05 15:16:25 by gmasid            #+#    #+#             */
-/*   Updated: 2022/10/22 17:34:20 by gmasid           ###   ########.fr       */
+/*   Updated: 2022/10/23 12:51:53 by gmasid           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINISHELL_H
 # define MINISHELL_H
 
-#include <readline/readline.h>
-#include <readline/history.h>
-#include <signal.h>
-#include <stdio.h>
+# include <readline/history.h>
+# include <readline/readline.h>
+# include <signal.h>
+# include <stdio.h>
 
 #endif
