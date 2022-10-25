413b32847594c8f99d199ffcbb2e68d9b3e81a24
Lucas Fads <lucasfads@gmail.com>
2022-10-25 17:18:34 -0300

    chore: error when wrong number of params for CD command

--- include/minishell.h
+++ include/minishell.h
@@ -6,7 +6,7 @@
 /*   By: lucafern <lucafern@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/05 15:16:25 by gmasid            #+#    #+#             */
-/*   Updated: 2022/10/25 16:50:35 by lucafern         ###   ########.fr       */
+/*   Updated: 2022/10/25 17:05:33 by lucafern         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,14 @@
 # define MINISHELL_H
 
 # include "../lib/libft/libft.h"
+# include <dirent.h>
 # include <limits.h>
 # include <readline/history.h>
 # include <readline/readline.h>
 # include <signal.h>
 # include <stdio.h>
 # include <stdlib.h>
+# include <sys/types.h>
 # include <sys/wait.h>
 # include <unistd.h>
 
--- src/builtins/cd.c
+++ src/builtins/cd.c
@@ -6,7 +6,7 @@
 /*   By: lucafern <lucafern@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/25 15:45:54 by lucafern          #+#    #+#             */
-/*   Updated: 2022/10/25 16:56:52 by lucafern         ###   ########.fr       */
+/*   Updated: 2022/10/25 17:13:58 by lucafern         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,10 @@ void	cd(char **command)
 	if (!command[1])
 		printf("Error: CD needs a path\n");
 	else if (!command[2])
-		chdir(command[1]);
+	{
+		if (chdir(command[1]))
+			perror("Error");
+	}
 	else
 		printf("Error: Too many arguments\n");
 }
