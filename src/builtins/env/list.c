3dae18a87d3404d09825003754c4b44e948d4967
Guilhermo Masid <guilhermomasid@gmail.com>
2022-10-29 09:11:22 -0300

    chore: add list file

---
+++ src/builtins/env/list.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   list.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gmasid <gmasid@student.42.rio>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/29 09:08:14 by gmasid            #+#    #+#             */
+/*   Updated: 2022/10/29 09:11:11 by gmasid           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../../includes/minishell.h"
+
+// t_env	*create_env(char **tab)
+// {
+// 	t_env	*env;
+// 	int		i;
+
+// 	env = NULL;
+// 	env = init_env(env);
+// 	env->env = tab;
+// 	i = 0;
+// 	while (tab[i])
+// 		add_env_elem(env, new_env_elem(tab[i++]));
+// 	return (env);
+// }
