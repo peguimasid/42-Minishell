07fa820759d72c0f7e450d3df07d04e4704956ee
Guilhermo Masid <guilhermomasid@gmail.com>
2022-10-29 12:32:14 -0300

    chore: set env struct on start program

--- src/main.c
+++ src/main.c
@@ -6,17 +6,18 @@
 /*   By: gmasid <gmasid@student.42.rio>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/22 17:31:19 by gmasid            #+#    #+#             */
-/*   Updated: 2022/10/29 12:29:13 by gmasid           ###   ########.fr       */
+/*   Updated: 2022/10/29 12:32:03 by gmasid           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
 
-void	init_data(t_data *data)
+void	init_data(t_data *data, char **env)
 {
 	data->running = true;
 	data->command = NULL;
 	data->env = NULL;
+	init_env(data, env);
 }
 
 int	main(int argc, char **argv, char **env)
@@ -27,7 +28,7 @@ int	main(int argc, char **argv, char **env)
 	(void)env;
 	if (argc != 1)
 		return (throw_error("This program accepts no arguments"));
-	init_data(&data);
+	init_data(&data, env);
 	launch(&data, env);
 	return (0);
 }
