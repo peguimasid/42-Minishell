4e64c4bc2adaf65d9baebcdda305a39fdaebd7a8
Guilhermo Masid <guilhermomasid@gmail.com>
2022-10-29 12:33:36 -0300

    chore: remove unnecessary line

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: gmasid <gmasid@student.42.rio>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/22 17:31:19 by gmasid            #+#    #+#             */
-/*   Updated: 2022/10/29 12:32:03 by gmasid           ###   ########.fr       */
+/*   Updated: 2022/10/29 12:33:14 by gmasid           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,7 +25,6 @@ int	main(int argc, char **argv, char **env)
 	t_data	data;
 
 	(void)argv;
-	(void)env;
 	if (argc != 1)
 		return (throw_error("This program accepts no arguments"));
 	init_data(&data, env);
