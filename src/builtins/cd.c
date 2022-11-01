e48a828462b749c54ab5bc5f2ddf25fda3ce4fda
Lucas Fads <lucasfads@gmail.com>
2022-11-01 14:43:27 -0300

    fix: PWD updates when cd (to home)

--- src/builtins/cd.c
+++ src/builtins/cd.c
@@ -6,28 +6,31 @@
 /*   By: lucafern <lucafern@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/26 12:46:30 by gmasid            #+#    #+#             */
-/*   Updated: 2022/11/01 14:29:52 by lucafern         ###   ########.fr       */
+/*   Updated: 2022/11/01 14:42:19 by lucafern         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minishell.h"
 
-void	update_pwd(t_data *data, char *new_path)
+void	update_pwd(t_data *data)
 {
+	char	cwd[PATH_MAX];
+
+	getcwd(cwd, sizeof(cwd));
 	set_env(data, ft_strdup("OLDPWD"), ft_strdup(get_env(data, "PWD")));
-	set_env(data, ft_strdup("PWD"), ft_strdup(new_path));
+	set_env(data, ft_strdup("PWD"), ft_strdup(cwd));
 }
 
 void	change_directory(t_data *data, char **args)
 {
 	char	*path;
-	char	cwd[PATH_MAX];
 
 	if (!args[1])
 	{
 		path = get_env(data, "HOME");
 		if (chdir(path) != 0)
 			perror("Error");
+		update_pwd(data);
 		return ;
 	}
 	if (args[2])
@@ -37,6 +40,5 @@ void	change_directory(t_data *data, char **args)
 	}
 	if (chdir(args[1]) != 0)
 		printf("%s: %s\n", strerror(errno), args[1]);
-	getcwd(cwd, sizeof(cwd));
-	update_pwd(data, cwd);
+	update_pwd(data);
 }
