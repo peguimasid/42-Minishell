8e2d1b23864168a33332323ffd58b4530a3bf31e
Guilhermo Masid <guilhermomasid@gmail.com>
2022-11-05 12:42:09 -0300

    chore: simply catch user input and print

--- src/minishell.c
+++ src/minishell.c
@@ -6,11 +6,23 @@
 /*   By: gmasid <gmasid@student.42.rio>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/05 12:02:30 by gmasid            #+#    #+#             */
-/*   Updated: 2022/11/05 12:21:10 by gmasid           ###   ########.fr       */
+/*   Updated: 2022/11/05 12:41:39 by gmasid           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
+
+void	input_loop(void)
+{
+	char	*str;
+
+	while (1)
+	{
+		str = readline("$");
+		printf("command was %s\n", str);
+		free(str);
+	}
+}
 
 void	minishell(int argc, char **argv, char **envp)
 {
@@ -20,4 +32,6 @@ void	minishell(int argc, char **argv, char **envp)
 		throw_error("Don't provider arguments");
 	signal(SIGINT, handle_sigint);
 	signal(SIGQUIT, SIG_IGN);
+	input_loop();
+	exit(g_status);
 }
