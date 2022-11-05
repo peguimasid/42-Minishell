effe6d8621bada7e08c633e80fa8f3e7c74bbfc4
Guilhermo Masid <guilhermomasid@gmail.com>
2022-11-05 13:12:23 -0300

    chore: create init data function

--- includes/minishell.h
+++ includes/minishell.h
@@ -6,7 +6,7 @@
 /*   By: gmasid <gmasid@student.42.rio>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/05 15:16:25 by gmasid            #+#    #+#             */
-/*   Updated: 2022/11/05 12:21:35 by gmasid           ###   ########.fr       */
+/*   Updated: 2022/11/05 12:58:42 by gmasid           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,15 +23,22 @@
 # include <sys/errno.h>
 # include <sys/wait.h>
 
-int		g_status;
+typedef struct s_data
+{
+	t_list	*cmds;
+	char	**envp;
+	pid_t	pid;
+}			t_data;
+
+int			g_status;
 
 // minishell.c
-void	minishell(int argc, char **argv, char **envp);
+void		minishell(int argc, char **argv, char **envp);
 
 // signal.c
-void	handle_sigint(int sig);
+void		handle_sigint(int sig);
 
 // common.c
-int		throw_error(char *error);
+int			throw_error(char *error);
 
 #endif
--- src/minishell.c
+++ src/minishell.c
@@ -6,16 +6,26 @@
 /*   By: gmasid <gmasid@student.42.rio>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/05 12:02:30 by gmasid            #+#    #+#             */
-/*   Updated: 2022/11/05 12:41:39 by gmasid           ###   ########.fr       */
+/*   Updated: 2022/11/05 13:11:47 by gmasid           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
 
-void	input_loop(void)
+t_data	init_data(char **argv, char **envp)
+{
+	// Duplicate enviroment variables
+	// Set global status as 0
+	// Start "cmds" as null
+	// Get process id (echo $$)
+}
+
+void	input_loop(char **argv, char **envp)
 {
 	char	*str;
+	t_data	data;
 
+	data = init_data(argv, envp);
 	while (1)
 	{
 		str = readline("$");
@@ -26,12 +36,10 @@ void	input_loop(void)
 
 void	minishell(int argc, char **argv, char **envp)
 {
-	(void)argv;
-	(void)envp;
 	if (argc != 1)
 		throw_error("Don't provider arguments");
 	signal(SIGINT, handle_sigint);
 	signal(SIGQUIT, SIG_IGN);
-	input_loop();
+	input_loop(argv, envp);
 	exit(g_status);
 }
