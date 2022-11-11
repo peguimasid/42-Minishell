7bd8fb57181a4639e112ee25428df1fbb9726219
Guilhermo Masid <guilhermomasid@gmail.com>
2022-11-11 17:17:43 -0300

    feature: add history

--- src/handle_input.c
+++ src/handle_input.c
@@ -6,7 +6,7 @@
 /*   By: gmasid <gmasid@student.42.rio>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/11 16:35:26 by gmasid            #+#    #+#             */
-/*   Updated: 2022/11/11 16:43:17 by gmasid           ###   ########.fr       */
+/*   Updated: 2022/11/11 17:16:57 by gmasid           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,10 @@
 
 int	handle_input(t_data *data)
 {
+	if (!data->input)
+		return (0);
+	add_history(data->input);
 	printf("command was %s\n", data->input);
 	free(data->input);
-	return (0);
+	return (1);
 }
