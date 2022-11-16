865a7ed075025189c0215cc716888e16845d8885
Guilhermo Masid <guilhermomasid@gmail.com>
2022-11-16 13:06:12 -0300

    chore: improving split method

--- src/parse/split_quotes.c
+++ src/parse/split_quotes.c
@@ -6,88 +6,85 @@
 /*   By: gmasid <gmasid@student.42.rio>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/15 12:50:36 by gmasid            #+#    #+#             */
-/*   Updated: 2022/11/15 14:40:13 by gmasid           ###   ########.fr       */
+/*   Updated: 2022/11/16 13:04:28 by gmasid           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minishell.h"
 
-int	ft_count_words(const char *s, char *c, int i[2])
+int	ft_count_words(const char *s, char *set, int count)
 {
 	int	q[2];
+	int	i;
 
+	i = 0;
 	q[0] = 0;
 	q[1] = 0;
-	while (s[i[0]] != '\0')
+	while (s && s[i] != '\0')
 	{
-		if (!ft_strchr(c, s[i[0]]))
+		count++;
+		if (!ft_strchr(set, s[i]))
 		{
-			i[1]++;
-			while ((!ft_strchr(c, s[i[0]]) || q[0]) && s[i[0]] != '\0')
+			while ((!ft_strchr(set, s[i]) || q[0] || q[1]) && s[i] != '\0')
 			{
-				if (!q[1] && (s[i[0]] == '\"' || s[i[0]] == '\''))
-					q[1] = s[i[0]];
-				q[0] = (q[0] + (s[i[0]] == q[1])) % 2;
-				q[1] *= q[0] != 0;
-				i[0]++;
+				q[0] = (q[0] + (!q[1] && s[i] == '\'')) % 2;
+				q[1] = (q[1] + (!q[0] && s[i] == '\"')) % 2;
+				i++;
 			}
-			if (q[0])
+			if (q[0] || q[1])
 				return (-1);
 		}
 		else
-			i[0]++;
+			i++;
 	}
-	return (i[1]);
+	return (count);
 }
 
-char	**ft_fill_array(char **aux, char const *s, char *set, int i[3])
+char	**ft_fill_array(char **aux, const char *s, char *set, int i[3])
 {
-	int	s_len;
 	int	q[2];
 
 	q[0] = 0;
 	q[1] = 0;
-	s_len = ft_strlen(s);
-	while (s[i[0]])
+	while (s && s[i[0]] != '\0')
 	{
 		while (ft_strchr(set, s[i[0]]) && s[i[0]] != '\0')
 			i[0]++;
 		i[1] = i[0];
-		while ((!ft_strchr(set, s[i[0]]) || q[0] || q[1]) && s[i[0]])
+		if (!ft_strchr(set, s[i[0]]))
 		{
-			q[0] = (q[0] + (!q[1] && s[i[0]] == '\'')) % 2;
-			q[1] = (q[1] + (!q[0] && s[i[0]] == '\"')) % 2;
-			i[0]++;
+			while ((!ft_strchr(set, s[i[0]]) || q[0] || q[1]) && s[i[0]])
+			{
+				q[0] = (q[0] + (!q[1] && s[i[0]] == '\'')) % 2;
+				q[1] = (q[1] + (!q[0] && s[i[0]] == '\"')) % 2;
+				i[0]++;
+			}
 		}
-		if (i[1] >= s_len)
-			aux[i[2]++] = "\0";
 		else
-			aux[i[2]++] = ft_substr(s, i[1], i[0] - i[1]);
+			i[0]++;
+		aux[i[2]++] = ft_substr(s, i[1], i[0] - i[1]);
 	}
 	return (aux);
 }
 
-char	**split_quotes(char const *s, char *set)
+char	**split_quotes(const char *s, char *set)
 {
-	char	**aux;
+	char	**result;
 	int		nwords;
 	int		i[3];
-	int		counts[2];
 
 	i[0] = 0;
 	i[1] = 0;
 	i[2] = 0;
-	counts[0] = 0;
-	counts[1] = 0;
 	if (!s)
 		return (NULL);
-	nwords = ft_count_words(s, set, counts);
+	nwords = ft_count_words(s, set, 0);
 	if (nwords == -1)
 		return (NULL);
-	aux = malloc((nwords + 1) * sizeof(char *));
-	if (aux == NULL)
+	result = malloc((nwords + 1) * sizeof(char *));
+	if (result == NULL)
 		return (NULL);
-	aux = ft_fill_array(aux, s, set, i);
-	aux[nwords] = NULL;
-	return (aux);
+	result = ft_fill_array(result, s, set, i);
+	result[nwords] = NULL;
+	return (result);
 }
