228c796207a35280e34d7a192d452ea44cf28824
Guilhermo Masid <guilhermomasid@gmail.com>
2022-11-16 13:59:45 -0300

    refactor: remove printlst function

--- src/handle_input.c
+++ src/handle_input.c
@@ -6,20 +6,11 @@
 /*   By: gmasid <gmasid@student.42.rio>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/11 16:35:26 by gmasid            #+#    #+#             */
-/*   Updated: 2022/11/16 12:49:54 by gmasid           ###   ########.fr       */
+/*   Updated: 2022/11/16 13:59:34 by gmasid           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
-
-void	print_lst(char **vec)
-{
-	int	i;
-
-	i = 0;
-	while (vec && vec[i])
-		printf(">> %s\n", vec[i++]);
-}
 
 int	handle_input(t_data *data)
 {
@@ -29,7 +20,6 @@ int	handle_input(t_data *data)
 		return (0);
 	add_history(data->input);
 	args = split_quotes(data->input, " ");
-	print_lst(args);
 	free(data->input);
 	if (!args)
 		return (throw_error(QUOTE, 1, NULL));
