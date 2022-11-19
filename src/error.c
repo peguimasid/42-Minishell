427a2ff7b785d38d2a49bd5914dc7509830a022e
Guilhermo Masid <guilhermomasid@gmail.com>
2022-11-19 14:36:46 -0300

    create print error message to bette vizulize errors

--- src/error.c
+++ src/error.c
@@ -6,19 +6,26 @@
 /*   By: gmasid <gmasid@student.42.rio>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/05 12:20:56 by gmasid            #+#    #+#             */
-/*   Updated: 2022/11/15 14:52:30 by gmasid           ###   ########.fr       */
+/*   Updated: 2022/11/19 14:35:57 by gmasid           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
 
+void	print_error(char *error)
+{
+	ft_putstr_fd("\033[0;31m\n", 2);
+	ft_putstr_fd(error, 2);
+	ft_putstr_fd("\033[0m", 2);
+}
+
 int	throw_error(int err_type, int error_code, char *message)
 {
 	g_status = error_code;
 	if (err_type == FORKERR)
-		ft_putstr_fd("minishell: fork failed\n", 2);
+		print_error("minishell: fork failed\n");
 	if (err_type == QUOTE)
-		ft_putstr_fd("minishell: not found matching quote\n", 2);
-	ft_putstr_fd(message, 2);
+		print_error("minishell: not found matching quote\n");
+	print_error(message);
 	return (1);
 }
