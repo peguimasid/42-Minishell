f24139291e97a51790f8979808e361f240506a8a
Guilhermo Masid <guilhermomasid@gmail.com>
2022-11-26 11:24:45 -0300

    create lexer function with todos

---
+++ src/lexer/main.c
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gmasid <gmasid@student.42.rio>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/26 11:17:14 by gmasid            #+#    #+#             */
+/*   Updated: 2022/11/26 11:19:22 by gmasid           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../includes/minishell.h"
+
+int	lexer(char *input, t_data *data)
+{
+	// TODO: Call split quotes
+	// TODO: Call function to expand args
+	// TODO: Call cmd_subsplit
+	// TODO: return (args);
+}
