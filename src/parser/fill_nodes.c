070e418e75fd26013d3117400bbd0d54e8033883
Guilhermo Masid <guilhermomasid@gmail.com>
2022-11-30 18:53:03 -0300

    improving method, still crash when send for example echo <<

--- src/parser/fill_nodes.c
+++ src/parser/fill_nodes.c
@@ -6,7 +6,7 @@
 /*   By: gmasid <gmasid@student.42.rio>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/30 17:56:26 by gmasid            #+#    #+#             */
-/*   Updated: 2022/11/30 17:57:04 by gmasid           ###   ########.fr       */
+/*   Updated: 2022/11/30 18:49:03 by gmasid           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,26 +26,50 @@ int	should_create_node(t_data *data, int i)
 	return (0);
 }
 
+int	fill_current_node(t_cmd *node, char **trimmed_args, t_data *data, int i)
+{
+	int		curr_arg;
+	char	*has_next_arg;
+	int		next_arg;
+
+	curr_arg = data->args[i][0];
+	has_next_arg = data->args[i + 1];
+	next_arg = data->args[i + 1][0];
+	if (!data->args[i])
+		return (0);
+	if (curr_arg == '>' && has_next_arg && next_arg == '>')
+		return (3);
+	if (curr_arg == '<' && has_next_arg && next_arg == '<')
+		return (3);
+	if (curr_arg == '>')
+		return (2);
+	if (curr_arg == '<')
+		return (2);
+	if (curr_arg != '|')
+		node->full_cmd = matrix_push(node->full_cmd, trimmed_args[i]);
+	return (1);
+}
+
 void	fill_nodes(t_data *data)
 {
 	char	**trimmed_args;
-	t_list	*aux;
+	t_list	*curr_node;
+	int		offset;
 	int		i;
 
 	trimmed_args = trim_args(data->args);
 	i = 0;
 	while (data->args[i])
 	{
-		aux = ft_lstlast(data->cmds);
+		curr_node = ft_lstlast(data->cmds);
 		if (should_create_node(data, i))
 		{
 			i += data->args[i][0] == '|';
 			ft_lstadd_back(&data->cmds, ft_lstnew(new_node()));
-			aux = ft_lstlast(data->cmds);
-			printf("create node\n");
+			curr_node = ft_lstlast(data->cmds);
 		}
-		(void)aux;
-		i++;
+		offset = fill_current_node(curr_node->content, trimmed_args, data, i);
+		i += offset;
 	}
 	free_matrix(trimmed_args);
 }
--- src/parser/main.c
+++ src/parser/main.c
@@ -6,13 +6,34 @@
 /*   By: gmasid <gmasid@student.42.rio>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/29 14:49:42 by gmasid            #+#    #+#             */
-/*   Updated: 2022/11/30 17:56:59 by gmasid           ###   ########.fr       */
+/*   Updated: 2022/11/30 18:14:05 by gmasid           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minishell.h"
 
+void	print_nodes(t_data *data)
+{
+	int		i;
+	t_cmd	*curr;
+
+	i = 1;
+	while (data->cmds)
+	{
+		curr = data->cmds->content;
+		printf("------- Node %d -------\n", i);
+		printf("full_cmd:\n");
+		print_matrix(curr->full_cmd);
+		printf("infile => %d\n", curr->infile);
+		printf("outfile => %d\n", curr->outfile);
+		printf("-----------------------\n\n");
+		data->cmds = data->cmds->next;
+		i++;
+	}
+}
+
 void	parse_args(t_data *data)
 {
 	fill_nodes(data);
+	print_nodes(data);
 }
