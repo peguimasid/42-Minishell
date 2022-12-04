8d76f3a81d81154882d3d8ea35c81e2219361314
Guilhermo Masid <guilhermomasid@gmail.com>
2022-12-04 13:12:41 -0300

    make sure command quit when type exit and not has commands after exit

--- src/executor/execute_commands.c
+++ src/executor/execute_commands.c
@@ -6,7 +6,7 @@
 /*   By: gmasid <gmasid@student.42.rio>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/04 11:33:27 by gmasid            #+#    #+#             */
-/*   Updated: 2022/12/04 13:02:08 by gmasid           ###   ########.fr       */
+/*   Updated: 2022/12/04 13:08:30 by gmasid           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,8 @@ int	handle_builtin(t_cmd *cmd)
 {
 	printf("BUILTIN\n");
 	printf("command => %s\n\n", *cmd->full_cmd);
+	if (ft_strncmp(*cmd->full_cmd, "exit", 4) == 0)
+		return (0);
 	return (1);
 }
 
@@ -31,18 +33,20 @@ int	execute_commands(t_data *data)
 	t_list	*current_node;
 	t_cmd	*current_command;
 	int		command_type;
+	int		status;
 
 	current_node = data->cmds;
+	status = 1;
 	while (current_node)
 	{
 		current_command = current_node->content;
 		command_type = get_command_type(current_command);
 		if (command_type == BUILTIN)
-			handle_builtin(current_command);
+			status = handle_builtin(current_command);
 		if (command_type == OTHER)
-			handle_command(current_command);
+			status = handle_command(current_command);
 		current_node = current_node->next;
 	}
 	wait_child_processes_exit(data);
-	return (1);
+	return (status);
 }
--- src/handle_input.c
+++ src/handle_input.c
@@ -6,7 +6,7 @@
 /*   By: gmasid <gmasid@student.42.rio>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/11 16:35:26 by gmasid            #+#    #+#             */
-/*   Updated: 2022/12/04 11:36:31 by gmasid           ###   ########.fr       */
+/*   Updated: 2022/12/04 13:09:50 by gmasid           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,16 @@
 
 int	handle_input(t_data *data)
 {
+	int	status;
+
+	status = 1;
 	if (is_invalid_input(data->input))
 		return (get_invalid_input_status(data->input));
 	add_history(data->input);
 	if (!lexer(data))
 		return (throw_error(QUOTE, 1, NULL));
 	parse_args(data);
-	executor(data);
+	status = executor(data);
 	clean(data);
-	return (1);
+	return (status);
 }
