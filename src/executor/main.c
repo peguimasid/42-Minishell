78df48d6b4736d7a0c49eebfe11cd825324ab135
Guilhermo Masid <guilhermomasid@gmail.com>
2022-12-04 11:38:36 -0300

    remove printg

--- src/executor/main.c
+++ src/executor/main.c
@@ -6,7 +6,7 @@
 /*   By: gmasid <gmasid@student.42.rio>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/04 11:33:27 by gmasid            #+#    #+#             */
-/*   Updated: 2022/12/04 11:37:12 by gmasid           ###   ########.fr       */
+/*   Updated: 2022/12/04 11:38:32 by gmasid           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,7 +38,6 @@ int	executor(t_data *data)
 {
 	if (!data->cmds)
 		return (0);
-	printf("GOT HERE\n");
 	print_nodes(data);
 	return (1);
 }
