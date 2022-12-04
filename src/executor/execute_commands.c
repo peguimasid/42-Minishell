49f9a328475facd11c3e427114f0ecf8474550e3
Guilhermo Masid <guilhermomasid@gmail.com>
2022-12-04 18:32:17 -0300

    check run from builtins to builtins that generate output

--- src/executor/execute_commands.c
+++ src/executor/execute_commands.c
@@ -6,31 +6,31 @@
 /*   By: gmasid <gmasid@student.42.rio>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/04 11:33:27 by gmasid            #+#    #+#             */
-/*   Updated: 2022/12/04 16:32:43 by gmasid           ###   ########.fr       */
+/*   Updated: 2022/12/04 18:28:05 by gmasid           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minishell.h"
 
-int	handle_builtin(t_cmd *cmd)
+int	handle_config_builtin(t_cmd *cmd)
 {
-	printf("BUILTIN\n");
+	printf("Change config\n");
 	printf("command => %s\n\n", *cmd->full_cmd);
 	return (1);
 }
 
-int	handle_extern(t_cmd *cmd)
+int	handle_others(t_cmd *cmd)
 {
-	printf("EXTERN\n");
+	printf("Generate output\n");
 	printf("command => %s\n\n", *cmd->full_cmd);
 	return (1);
 }
 
 int	handle_command(t_cmd *command)
 {
-	if (is_builtin(command))
-		return (handle_builtin(command));
-	return (handle_extern(command));
+	if (is_config_builtin(command))
+		return (handle_config_builtin(command));
+	return (handle_others(command));
 }
 
 int	execute_commands(t_data *data)
--- src/executor/utils.c
+++ src/executor/utils.c
@@ -6,7 +6,7 @@
 /*   By: gmasid <gmasid@student.42.rio>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/04 12:35:55 by gmasid            #+#    #+#             */
-/*   Updated: 2022/12/04 15:44:09 by gmasid           ###   ########.fr       */
+/*   Updated: 2022/12/04 18:24:40 by gmasid           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,6 +34,28 @@ int	is_builtin(t_cmd *node)
 	if (ft_strncmp(cmd, "unset", ft_strlen(cmd)) == 0 && ft_strlen(cmd) == 5)
 		return (1);
 	if (ft_strncmp(cmd, "echo", ft_strlen(cmd)) == 0 && ft_strlen(cmd) == 4)
+		return (1);
+	if (ft_strncmp(cmd, "exit", ft_strlen(cmd)) == 0 && ft_strlen(cmd) == 4)
+		return (1);
+	return (0);
+}
+
+int	is_config_builtin(t_cmd *node)
+{
+	char	*cmd;
+
+	cmd = node->full_cmd[0];
+	if (!cmd)
+		return (0);
+	if (ft_strchr(cmd, '/'))
+		return (0);
+	if (node->cmd_path && ft_strchr(node->cmd_path, '/'))
+		return (0);
+	if (ft_strncmp(cmd, "cd", ft_strlen(cmd)) == 0 && ft_strlen(cmd) == 2)
+		return (1);
+	if (ft_strncmp(cmd, "export", ft_strlen(cmd)) == 0 && ft_strlen(cmd) == 6)
+		return (1);
+	if (ft_strncmp(cmd, "unset", ft_strlen(cmd)) == 0 && ft_strlen(cmd) == 5)
 		return (1);
 	if (ft_strncmp(cmd, "exit", ft_strlen(cmd)) == 0 && ft_strlen(cmd) == 4)
 		return (1);
