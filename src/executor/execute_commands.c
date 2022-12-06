2b85c2d1ccf6c0412b50326c2b0bb7b2cafa16b6
Guilhermo Masid <guilhermomasid@gmail.com>
2022-12-06 12:44:57 -0300

    refactor: send entire node instead just command to handle command function

--- src/executor/execute_commands.c
+++ src/executor/execute_commands.c
@@ -6,17 +6,20 @@
 /*   By: gmasid <gmasid@student.42.rio>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/04 11:33:27 by gmasid            #+#    #+#             */
-/*   Updated: 2022/12/06 12:25:50 by gmasid           ###   ########.fr       */
+/*   Updated: 2022/12/06 12:45:30 by gmasid           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minishell.h"
 
-int	handle_command(t_cmd *command, t_data *data, int has_next)
+int	handle_command(t_list *node, t_data *data)
 {
+	t_cmd	*command;
+
+	command = node->content;
 	if (!command->full_cmd)
 		return (1);
-	if (is_config_builtin(command) && !has_next)
+	if (is_config_builtin(command) && !has_next(node))
 		return (handle_config_builtin(command, data));
 	return (handle_generate_output(command, data));
 }
@@ -24,15 +27,13 @@ int	handle_command(t_cmd *command, t_data *data, int has_next)
 int	execute_commands(t_data *data)
 {
 	t_list	*current_node;
-	t_cmd	*command;
 	int		status;
 
 	current_node = data->cmds;
 	status = 1;
 	while (current_node)
 	{
-		command = current_node->content;
-		status = handle_command(command, data, has_next(current_node));
+		status = handle_command(current_node, data);
 		current_node = current_node->next;
 	}
 	wait_child_processes_exit(data);
