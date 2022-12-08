23963c1e41109579cb76aba85c74be8ecd4e0135
Guilhermo Masid <guilhermomasid@gmail.com>
2022-12-08 17:47:04 -0300

    remove unnecessary print char from print_matrix function

--- src/utils/matrix/matrix_print.c
+++ src/utils/matrix/matrix_print.c
@@ -6,7 +6,7 @@
 /*   By: gmasid <gmasid@student.42.rio>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/26 12:35:44 by gmasid            #+#    #+#             */
-/*   Updated: 2022/11/26 12:47:09 by gmasid           ###   ########.fr       */
+/*   Updated: 2022/12/08 17:46:19 by gmasid           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ void	print_matrix(char **matrix)
 	i = 0;
 	while (matrix && matrix[i])
 	{
-		printf(">> matrix[%d] = %s\n", i, matrix[i]);
+		printf("%s\n", matrix[i]);
 		i++;
 	}
 }
