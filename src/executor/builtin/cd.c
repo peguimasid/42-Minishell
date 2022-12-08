3007f4e3f8cd0e3ad2e021c563118eff3d971458
Guilhermo Masid <guilhermomasid@gmail.com>
2022-12-08 18:02:37 -0300

    remove second paramter checking in cd method

--- src/executor/builtin/cd.c
+++ src/executor/builtin/cd.c
@@ -6,7 +6,7 @@
 /*   By: gmasid <gmasid@student.42.rio>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/08 16:50:58 by gmasid            #+#    #+#             */
-/*   Updated: 2022/12/08 17:54:12 by gmasid           ###   ########.fr       */
+/*   Updated: 2022/12/08 18:01:35 by gmasid           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,11 +25,6 @@ int	execute_cd(t_cmd *cmd, t_data *data)
 		if (chdir(path) != 0)
 			perror("Error");
 		free(path);
-		return (1);
-	}
-	if (cmd->full_cmd[2])
-	{
-		printf("cd: string not in pwd: %s\n", cmd->full_cmd[1]);
 		return (1);
 	}
 	if (chdir(cmd->full_cmd[1]) != 0)
