d51c5107ff6128b3e1dbc4174c0d11343a648d81
Guilhermo Masid <guilhermomasid@gmail.com>
2022-12-09 19:11:40 -0300

    feature: implement change directory (cd)

--- src/executor/builtin/cd.c
+++ src/executor/builtin/cd.c
@@ -6,30 +6,52 @@
 /*   By: gmasid <gmasid@student.42.rio>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/08 16:50:58 by gmasid            #+#    #+#             */
-/*   Updated: 2022/12/08 18:01:35 by gmasid           ###   ########.fr       */
+/*   Updated: 2022/12/09 19:04:48 by gmasid           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../../includes/minishell.h"
 
-// TODO: This is temporary, we still need to set PWD and OLDPWD vars
-int	execute_cd(t_cmd *cmd, t_data *data)
+void	handle_set_pwd_vars(t_data *data)
 {
-	char	*path;
-	char	cwd[PATH_MAX];
+	char	*cwd;
+	char	*current_pwd;
 
-	getcwd(cwd, sizeof(cwd));
-	if (!cmd->full_cmd[1])
-	{
-		path = get_env("HOME", data->envp, 4);
-		if (chdir(path) != 0)
-			perror("Error");
-		free(path);
-		return (1);
-	}
+	cwd = getcwd(NULL, 0);
+	current_pwd = get_env("PWD", data->envp, 3);
+	set_env("OLDPWD", current_pwd, data->envp);
+	set_env("PWD", cwd, data->envp);
+	free(current_pwd);
+	free(cwd);
+}
+
+int	handle_change_dir_to_home(t_data *data)
+{
+	char	*home;
+
+	home = get_env("HOME", data->envp, 4);
+	if (!home)
+		return (throw_error(0, 1, "minishell: cd: HOME not set"));
+	if (chdir(home) != 0)
+		perror("Error");
+	free(home);
+	handle_set_pwd_vars(data);
+	return (1);
+}
+
+int	handle_change_dir(t_cmd *cmd, t_data *data)
+{
 	if (chdir(cmd->full_cmd[1]) != 0)
 		printf("%s: %s\n", strerror(errno), cmd->full_cmd[1]);
+	handle_set_pwd_vars(data);
 	return (1);
+}
+
+int	execute_cd(t_cmd *cmd, t_data *data)
+{
+	if (!cmd->full_cmd[1])
+		return (handle_change_dir_to_home(data));
+	return (handle_change_dir(cmd, data));
 }
 
 int	is_cd(t_cmd *node)
