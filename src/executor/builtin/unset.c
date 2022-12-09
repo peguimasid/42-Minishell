6036ce2f842bd12e364df7b308518345b65dd7a8
Guilhermo Masid <guilhermomasid@gmail.com>
2022-12-09 20:10:14 -0300

    unset working

--- src/executor/builtin/unset.c
+++ src/executor/builtin/unset.c
@@ -6,7 +6,7 @@
 /*   By: gmasid <gmasid@student.42.rio>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/09 19:47:54 by gmasid            #+#    #+#             */
-/*   Updated: 2022/12/09 19:49:26 by gmasid           ###   ########.fr       */
+/*   Updated: 2022/12/09 19:59:39 by gmasid           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,16 @@
 
 int	execute_unset(t_cmd *cmd, t_data *data)
 {
-	(void)cmd;
-	(void)data;
-	printf("Unset");
+	char	*env_to_remove;
+	int		index_to_remove;
+
+	if (!cmd || !cmd->full_cmd[1])
+		return (1);
+	env_to_remove = cmd->full_cmd[1];
+	index_to_remove = get_env_index(env_to_remove, data->envp);
+	if (index_to_remove < 0)
+		return (1);
+	data->envp = matrix_remove(data->envp, index_to_remove);
 	return (1);
 }
 
