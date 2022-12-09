c7319db6dadd2263a10013504837bf62eb286ade
Lucas Fads <lucasfads@gmail.com>
2022-12-09 19:39:40 -0300

    fix: free_matrix in output builtins

--- src/executor/builtin/main.c
+++ src/executor/builtin/main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: gmasid <gmasid@student.42.rio>             +#+  +:+       +#+        */
+/*   By: lucafern <lucafern@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/05 18:18:27 by gmasid            #+#    #+#             */
-/*   Updated: 2022/12/09 11:56:28 by gmasid           ###   ########.fr       */
+/*   Updated: 2022/12/09 19:37:43 by lucafern         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,8 +32,9 @@ int	handle_generate_output_builtin(t_list *node, t_data *data)
 
 	command = node->content;
 	if (is_env(command))
-		return (execute_env(data));
+		execute_env(data);
 	if (is_echo(command))
-		return (execute_echo(command));
+		execute_echo(command);
+	free_matrix(data->envp);
 	return (1);
 }
